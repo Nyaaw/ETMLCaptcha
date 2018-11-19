624bbf05ffd38ebebcd4e22a71097b57922bba8b
Sherokhi <jeremie.perret.vuarrengel2000@gmail.com>
2018-11-19 11:04:44 +0100

    New class inside

--- ETMLCaptcha.js
+++ ETMLCaptcha.js
@@ -10,13 +10,14 @@
 import("https://redom.js.org/redom.min.js");
 
 var config = {
-    container_id = "captcha_container"
+    container_id = "captcha_container",
+    type = {CALCULATIONS = "calculations", PICTURES = "pictures", TEXT = "text"}
 };
 
 /**
  * Constructeur de la librairies
  */
-function ETMLCaptcha(){
+function ETMLCaptcha(type = ""){
 
 }
 
@@ -25,4 +26,20 @@ function ETMLCaptcha(){
  */
 function OutputDesign(afterWhat = "header", insideWhat = "body"){
 
+}
+
+/**
+ * Génère un captcha selon le type choisi
+ */
+function GenerateCaptcha(type = config.type.CALCULATIONS){
+    switch(type){
+        case config.type.CALCULATIONS:
+            break;
+    }
+}
+
+class Captcha{
+    constructor(id, type, data){
+        
+    }
 }
