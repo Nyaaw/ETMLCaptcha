cc80723d50fdcb7a15a9fa17eeb9338afcba8584
Sherokhi <jeremie.perret.vuarrengel2000@gmail.com>
2018-11-19 10:22:43 +0100

    Première structure de la librairie

---
+++ ETMLCaptcha.js
@@ -0,0 +1,18 @@
+/**
+*	ETML
+*	Auteur: perretje - ripollpi
+*	Date : 19.11.2018
+*	Description : Librairie JavaScript pour ajouter un Captcha HomeMade
+*/
+
+var config = {
+      
+};
+
+/**
+ * Constructeur de la librairies
+ */
+function ETMLCaptcha(){
+
+}
+
