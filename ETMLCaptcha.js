6defedd24afe0ac0a568cfd9a22248f796bc3841
Nyaaw <cactus318@hotmail.fr>
2018-11-26 08:49:39 +0100

    fix du commit et de npm

--- ETMLCaptcha.js
+++ ETMLCaptcha.js
@@ -7,14 +7,40 @@
 
 import { el, mount } from 'modules/redom.js';
 
-var config = {
-      
+
+
+var constants = {
+    container_id = "captcha_container",
+    type = "calculations" // "calculations", "pictures" ou "text"
+
 };
 
 /**
  * Constructeur de la librairies
  */
-function ETMLCaptcha(){
-
+function ETMLCaptcha(container_id = "captcha_container", type = "calculations"){
+    constants.container_id = container_id;
+    constants.type = type;
 }
 
+/**
+ * Génère un captcha selon le type choisi
+ */
+function GenerateCaptcha(type){
+    switch(type){
+        case constants.type.calculations:
+            break;
+
+        case constants.type.pictures:
+            break;
+            
+        case constants.type.text:
+            break;
+    }
+}
+
+class Captcha{
+    constructor(id, type, data){
+        
+    }
+}
